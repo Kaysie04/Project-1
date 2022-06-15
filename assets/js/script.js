6a9c34390e9131c8d492ddd90a38d30d3edcdda1
Adrian Alaniz <adrian512@Adrians-MBP.localdomain>
2022-06-15 15:53:39 -0500

    html set up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,18 @@
 
+let map: google.maps.Map;
+
+function initMap(): void {
+  map = new google.maps.Map(document.getElementById("map").HTMLElement {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+window.initMap = initMap;
+export {};
 

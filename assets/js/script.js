89eb0a1b1382db208214e86c22794f8bf601ce09
Kaysie <kaysielandersonwork@gmail.com>
2022-06-17 13:49:04 -0500

    display forecast data

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,6 @@ var getAddress= document.getElementById("getAddress")
 var getHours = document.getElementById("getHours")
 var entranceFee = document.getElementById("getEntryFee")
 var parkWeatherInfo = document.getElementById("park-weather-info")
-var weatherDataEl = document.getElementById("testing-weather-data")
 var searchInputEl = document.getElementById("user-search")
 var searchBtn = document.getElementById("search-btn")
 const APIKeyWeather = "a4d995d10a3e4d37b4522008221606"
@@ -31,9 +30,7 @@ function getPark (userSearch) {
             var lat = data.data[0].latitude
             var long = data.data[0].longitude
             var latLong = `${lat},${long}`
-            console.log(latLong)
-           
-           
+            
             // create a variable and element for the park name and append it to the correct html div
             getParkName.innerHTML = data.data[0].fullName
 
@@ -56,38 +53,75 @@ function getPark (userSearch) {
               getEntryFee.innerHTML = `Entrance Fee: $${parkCost} ${parkDescription}`
 
               
-        var weatherUrl = `http://api.weatherapi.com/v1/forecast.json?key=${APIKeyWeather}&q=${latLong}`
+        var weatherUrl = `http://api.weatherapi.com/v1/forecast.json?key=${APIKeyWeather}&q=${latLong}&days=7`
 
         fetch(weatherUrl)
             .then(response => {
                 return response.json() })
                 .then(data => {
                     console.log(data)
+                    
                 // icon
                 // var weatherIcon = data.current.condition.icon
                 // weatherIconImg = document.createElement("img")
                 // weatherIconImg.setAttribute("src", "//cdn.weatherapi.com/weather/64x64/night/113.png")
                 // weatherDataEl.append(weatherIconImg)
+
+               
+               
+                //TODAY'S WEATHER
+
                 // current temp
-                var currentTemp = document.createElement("p")
+                var currentTemp = document.getElementById("currentTemp")
                 currentTemp.innerHTML = ` Temp: ${data.current.temp_f} ${"\u00B0F"}`
-                weatherDataEl.append(currentTemp)
+        
                 // feels like temp
-                var feelsLike = document.createElement("p")
+                var feelsLike = document.getElementById("feelsLike")
                 feelsLike.innerHTML = ` Feels Like: ${data.current.feelslike_f} ${"\u00B0F"}`
-                weatherDataEl.append(feelsLike)
+             
                 // humidity
-                var humidity = document.createElement("p")
+                var humidity = document.getElementById("humidity")
                 humidity.innerHTML = ` Humidity: ${data.current.humidity}${"%"}`
-                weatherDataEl.append(humidity)
+    
                 // wind
-                var wind = document.createElement("p")
+                var wind = document.getElementById("wind")
                 wind.innerHTML = ` Wind: ${data.current.wind_mph}${"mph"}`
-                weatherDataEl.append(wind)
+
                 // uv
-                var wind = document.createElement("p")
-                wind.innerHTML = `UV Index: ${data.current.uv}`
-                weatherDataEl.append(wind)
+                var uvIndex = document.getElementById("uv")
+                uvIndex.innerHTML = `UV Index: ${data.current.uv}`
+
+                // DAY 1 WEATHER
+                var dayOneDate = document.getElementById("day1-date")
+                dayOneDate.innerHTML = `${data.forecast.forecastday[1].date}`
+
+                var dayOneTemp = document.getElementById("day1-temp")
+                dayOneTemp.innerHTML = ` Average Temp: ${data.forecast.forecastday[1].day.avgtemp_f}${"\u00B0F"}`
+
+                var dayOneHumidity = document.getElementById("day1-humidity")
+                dayOneHumidity.innerHTML = `Humidity: ${data.forecast.forecastday[1].day.avghumidity}${"%"}`
+
+                var dayOneWind = document.getElementById("day1-wind")
+                dayOneWind.innerHTML = `Wind Speed: ${data.forecast.forecastday[1].day.maxwind_mph}${"mph"}`
+
+                var dayOneUv = document.getElementById("day1-uv")
+                dayOneUv.innerHTML = `UV Index: ${data.forecast.forecastday[1].day.uv}`
+
+                 // DAY 2 WEATHER
+                 var dayTwoDate = document.getElementById("day2-date")
+                 dayTwoDate.innerHTML = `${data.forecast.forecastday[2].date}`
+ 
+                 var dayTwoTemp = document.getElementById("day2-temp")
+                 dayTwoTemp.innerHTML = ` Average Temp: ${data.forecast.forecastday[2].day.avgtemp_f}${"\u00B0F"}`
+ 
+                 var dayTwoHumidity = document.getElementById("day2-humidity")
+                 dayTwoHumidity.innerHTML = `Humidity: ${data.forecast.forecastday[2].day.avghumidity}${"%"}`
+ 
+                 var dayTwoWind = document.getElementById("day2-wind")
+                 dayTwoWind.innerHTML = `Wind Speed: ${data.forecast.forecastday[2].day.maxwind_mph}${"mph"}`
+ 
+                 var dayTwoUv = document.getElementById("day2-uv")
+                 dayTwoUv.innerHTML = `UV Index: ${data.forecast.forecastday[2].day.uv}`
             })
             
          })
